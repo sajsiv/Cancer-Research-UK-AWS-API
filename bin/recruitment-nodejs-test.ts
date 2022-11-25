4e62cc3abbce146221fa0623637d4355a54fa03e
Sajjan Sivia <sajjansivia@gmail.com>
2022-11-25 13:36:16 +0000

    deploying stack to my AWS account

--- bin/recruitment-nodejs-test.ts
+++ bin/recruitment-nodejs-test.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+//import * as cdk from '@aws-cdk/core';
+import { CfnOutput, App, Fn, Tags, Stack, StackProps } from 'aws-cdk-lib';
 import { RecruitmentNodejsTestStack } from '../lib/recruitment-nodejs-test-stack';
 
-const app = new cdk.App();
+const app = new App();
 new RecruitmentNodejsTestStack(app, 'RecruitmentNodejsTestStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -15,7 +16,7 @@ new RecruitmentNodejsTestStack(app, 'RecruitmentNodejsTestStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '125624627088', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
