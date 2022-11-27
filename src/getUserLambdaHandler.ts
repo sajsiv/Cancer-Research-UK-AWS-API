f358520acb49369682f6941833515fc5bee269df
Sajjan Sivia <sajjansivia@gmail.com>
2022-11-27 00:25:32 +0000

    making getUserLambdaHandler get the email address specified in the query params

--- src/getUserLambdaHandler.ts
+++ src/getUserLambdaHandler.ts
@@ -2,7 +2,7 @@
 import * as types from './types';
 
 // Get context type
-import{ Context } from 'aws-lambda';
+import{ Context, APIGatewayEvent } from 'aws-lambda';
 
 
 
@@ -14,20 +14,21 @@ const dynamodb = require('aws-sdk/clients/dynamodb')
 const documentClient = new dynamodb.DocumentClient();
 
 // Get method which returns all user info from the DynamoDB table
-exports.getUserLambdaHandler = async (event: types.GetUserByEmailEvent, context: Context) => {
+exports.getUserLambdaHandler = async (event: APIGatewayEvent, context: Context) => {
     // Logs to CloudWatch
     console.info('Received request to get user:', context, event)
 
     var params = {
-        TableName: tableName
+        TableName: tableName,
+        Key: event.queryStringParameters
     }
 
-    const data = await documentClient.scan(params).promise();
-    const items = data.Items;
+    const data = await documentClient.get(params).promise();
+    const item = data.Item;
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify(item)
     }
 
     //Logs to CloudWatch
