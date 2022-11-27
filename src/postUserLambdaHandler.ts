8a412f63d47314fba1331864d271861f13b2443e
Sajjan Sivia <sajjansivia@gmail.com>
2022-11-27 16:51:49 +0000

    adding error handling to post user lambda

--- src/postUserLambdaHandler.ts
+++ src/postUserLambdaHandler.ts
@@ -3,8 +3,9 @@ import * as types from './types';
 
 // Get context type
 import{ Context, APIGatewayEvent } from 'aws-lambda';
-import { Identity } from 'aws-cdk-lib/aws-ses';
-import { userInfo } from 'os';
+
+// Get validate email function
+import { validateEmail } from './utils/validateEmail';
 
 // Get DynamoDB table name from env variables
 const tableName = process.env.USER_TABLE_NAME;
@@ -25,21 +26,54 @@ exports.postUserLambdaHandler = async (event: APIGatewayEvent, context: Context)
     
     console.info( 'UTC: [', (new Date()).toUTCString, '] Body email:', email_address);
 
+
+    if(!tableName){
+        const error = "Table name is undefined"
+        console.error(error, context);
+        throw new Error(error);
+    };
+
+    
+    if(!user){
+        const error = "user is undefined, the email address and an empty donations array must be packed in a user object when posting a new user"
+        console.error(error, context);
+        throw new Error(error);
+    }
+
+    if(!email_address){
+        const error = "email_address is undefined, it is a required when posting a new user"
+        console.error(error, context);
+        throw new Error(error);
+    };
+
+    const validEmail = validateEmail(email_address);
+
+    if (!validEmail){
+        const error = "email_address is invalid, ensure a valid email address is being posted"
+        console.error(error, context);
+        throw new Error(error);
+    };
+
     var params = {
         TableName: tableName,
         Item: user
     }
 
-    const data = await documentClient.put(params).promise();
-    const items = data.Items;
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(body)
-    }
-
-    //Logs to CloudWatch
-    console.info(`response from: ${context.clientContext?.client} statusCode: ${response.statusCode} body: ${response.body}`)
+    try {
+        const data = await documentClient.put(params).promise();
     
-    return response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        }
+    
+        //Logs to CloudWatch
+        console.info(`response from: ${context.clientContext?.client} statusCode: ${response.statusCode} body: ${response.body}`)
+        
+        return response
+    
+    }catch (err){
+        console.error(err, context);
+        throw err;
+    }
 }
